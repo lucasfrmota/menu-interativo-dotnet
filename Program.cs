63e74a789773a041cff7ad16c1c93f7235b77ec0
lucasfrmota <lucasfrmota@gmail.com>
2024-08-04 10:07:34 -0300

    Criação do menu interativo finalizado

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+//Declaração as variaveis de suporte
+
+string opcao;
+bool exibirMenu = true;
+
+
+// Laço 
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch(opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+
+        case "2":
+            Console.WriteLine("Busca de cliente");
+            break;
+
+        case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+
+        case "4":
+            Console.WriteLine("Encerrar");
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;    
+    }
+}
+
+Console.WriteLine("O programa se encerrou");
